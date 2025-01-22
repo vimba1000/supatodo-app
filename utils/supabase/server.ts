8f00f7100426d34bd1b74dda87fea90c14f1131c
vimba1000 <vimbainashetrinity229@gmail.com>
2025-01-22 04:42:16 -0800

    initial commit

---
+++ utils/supabase/server.ts
@@ -0,0 +1,37 @@
+import { Database } from '@/types/supabase'
+import { createServerClient, type CookieOptions } from '@supabase/ssr'
+import { cookies } from 'next/headers'
+
+export function createClient() {
+    const cookieStore = cookies()
+
+    return createServerClient<Database>(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+        {
+            cookies: {
+                async get(name: string) {
+                    return (await cookieStore).get(name)?.value
+                },
+                async set(name: string, value: string, options: CookieOptions) {
+                    try {
+                        (await cookieStore).set({ name, value, ...options })
+                    } catch (error) {
+                        // The `set` method was called from a Server Component.
+                        // This can be ignored if you have middleware refreshing
+                        // user sessions.
+                    }
+                },
+                async remove(name: string, options: CookieOptions) {
+                    try {
+                        (await cookieStore).set({ name, value: '', ...options })
+                    } catch (error) {
+                        // The `delete` method was called from a Server Component.
+                        // This can be ignored if you have middleware refreshing
+                        // user sessions.
+                    }
+                },
+            },
+        }
+    )
+}
